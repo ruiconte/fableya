6b9b381bc80d9d9b90e5357a444aa0b06c8b7503
Louis <ruiconte@gmail.com>
2026-08-24 12:47:08 +0200

    revert: remove side decorations

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,9 @@
 import { Outlet } from 'react-router-dom'
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
-import { SideDecorations } from './SideDecorations'
-
 export function Layout() {
   return (
     <div className="min-h-screen flex flex-col">
-      <SideDecorations />
       <Navbar />
       <main className="flex-1">
         <Outlet />
